f573682198c5b335d92a115493634bded75b125e
anniluo <anniluo@metropolia.fi>
2020-01-21 20:27:38 +0200

    modal setup

---
+++ src/components/modal/modalComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Modal = () => {
+  return (
+    <div>
+      <h2>Modal</h2>
+      <p>This is going to be a reusable Modal component</p>
+    </div>
+  );
+};
+
+export default Modal;
