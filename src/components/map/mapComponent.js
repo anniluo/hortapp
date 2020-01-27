115eb586bf2b03206230e2ccf84f1bf82c2a3669
anniluo <anniluo@metropolia.fi>
2020-01-27 18:25:25 +0200

    get location-button and add nature resource button setup

--- src/components/map/mapComponent.js
+++ src/components/map/mapComponent.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, ZoomControl } from 'react-leaflet';
 import mockMarkers from '../../utils/mockNatureResourceMarker';
 import NatureResourceMarker from '../natureResourceMarker/natureResourceMarker';
+import LeafletControlButton from '../button/buttonComponent';
 
 const LeafletMap = () => {
   const mapPosition = [60.192059, 24.945831];
@@ -13,12 +14,21 @@ const LeafletMap = () => {
 
   return (
     <>
-      <Map center={mapPosition} zoom={13}>
+      <Map center={mapPosition} zoom={13} zoomControl={false}>
+        <ZoomControl position="bottomright"></ZoomControl>
+        <LeafletControlButton
+          buttonPosition="topright"
+          buttonText="Add Resource"
+        ></LeafletControlButton>
+        <LeafletControlButton
+          buttonPosition="topright"
+          buttonText="Get Location"
+        ></LeafletControlButton>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <div className="marker-container">{renderNatureResourceMarkers()}</div>
+        <div className="marker-container">{renderNatureResourceMarkers()}</div>;
       </Map>
     </>
   );
