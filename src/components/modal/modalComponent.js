b982c4caaa48602ab61d05ac6e42e918a3606ec0
anniluo <anniluo@metropolia.fi>
2020-01-29 22:08:49 +0200

    modal ui elements

--- src/components/modal/modalComponent.js
+++ src/components/modal/modalComponent.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import './modalComponent.css';
 
 const Modal = () => {
   return (
-    <div>
-      <h2>Modal</h2>
-      <p>This is going to be a reusable Modal component</p>
+    <div className="modal-container">
+      <div className="modal-buttons-container">
+        <button id="login-button">Login</button>
+        <button id="signup-button">Sign Up</button>
+      </div>
+      <form className="modal-form">
+        <input className="modal-form-input" type="text" placeholder="username"></input>
+        <input className="modal-form-input" type="email" placeholder="e-mail"></input>
+        <input className="modal-form-input" type="text" placeholder="password"></input>
+        <input className="modal-form-input" type="submit" value="submit"></input>
+      </form>
     </div>
   );
 };
