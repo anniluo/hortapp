21388fc03df424e99f37d8deef703b01b5f36591
anniluo <anniluo@metropolia.fi>
2020-02-10 17:40:34 +0200

    ui for signup and login modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import LeafletMap from './components/map/mapComponent';
+import Modal from './components/modal/modalComponent';
 
 function App() {
   return (
     <div className="App">
-      <LeafletMap></LeafletMap>
+      <Modal></Modal>
     </div>
   );
 }
--- src/components/modal/modalComponent.js
+++ src/components/modal/modalComponent.js
@@ -1,19 +1,43 @@
 import React from 'react';
 import './modalComponent.css';
 
+//needs a state to determine which is chosen (signup or login)
+
 const Modal = () => {
+  const signupForm = () => {
+    return (
+      <form id="singup-form" className="modal-form">
+        <input className="modal-form-input" type="email" placeholder="e-mail"></input>
+        <input className="modal-form-input" type="text" placeholder="username"></input>
+        <input className="modal-form-input" type="text" placeholder="password"></input>
+        <input className="modal-form-input" type="text" placeholder="confirm password"></input>
+        <input className="modal-form-input round-button" type="submit" value="Confirm"></input>
+      </form>
+    );
+  };
+
+  const loginForm = () => {
+    return (
+      <form id="login -form" className="modal-form">
+        <input className="modal-form-input" type="text" placeholder="username"></input>
+        <input className="modal-form-input" type="text" placeholder="password"></input>
+        <input className="modal-form-input round-button" type="submit" value="Confirm"></input>
+      </form>
+    );
+  };
+
   return (
     <div className="modal-container">
       <div className="modal-buttons-container">
-        <button id="login-button">Login</button>
-        <button id="signup-button">Sign Up</button>
+        <button id="login-button" className="modal-top-buttons">
+          Login
+        </button>
+        <button id="signup-button" className="modal-top-buttons">
+          Sign Up
+        </button>
+        <button id="close-modal-button" className="modal-top-buttons"></button>
       </div>
-      <form className="modal-form">
-        <input className="modal-form-input" type="text" placeholder="username"></input>
-        <input className="modal-form-input" type="email" placeholder="e-mail"></input>
-        <input className="modal-form-input" type="text" placeholder="password"></input>
-        <input className="modal-form-input" type="submit" value="submit"></input>
-      </form>
+      {loginForm()}
     </div>
   );
 };
