93a7fd994f5e12d63c76e11132a005eda2a0dd89
anniluo <anniluo@metropolia.fi>
2020-02-12 13:53:54 +0200

    menu ui setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import './App.css';
 import LeafletMap from './components/map/mapComponent';
-
-const AddNatureResourceButton = () => {
-  return <button id="add-button">Add Resource</button>;
-};
-
-const UsersLocationButton = () => {
-  /*TODO: 
-  1. ask permission to get location of the device
-  2. get user's location and recentering the map*/
-  return <button id="location-button">Location</button>;
-};
+import HortappMenu from './components/menu/menuComponent';
 
 function App() {
   return (
-    <div className="App">
-      <LeafletMap></LeafletMap>
+    <div className='App'>
+      <HortappMenu></HortappMenu>
     </div>
   );
 }
