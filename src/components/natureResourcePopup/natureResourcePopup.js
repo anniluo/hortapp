a382e5d2da0eba76b31f95031098f4949c5fd780
anniluo <anniluo@metropolia.fi>
2020-02-19 22:24:48 +0200

    resource popup fix

--- src/components/natureResourcePopup/natureResourcePopup.js
+++ src/components/natureResourcePopup/natureResourcePopup.js
@@ -6,14 +6,22 @@ import fruitsAndVegetableIcon from '../../assets/fruits-and-vegetables.png';
 
 const NatureResourcePopup = ({ mockMarker }) => {
   return (
-    <Popup>
-      <img className='resource-icon' src={fruitsAndVegetableIcon} alt='Icon for berries' />
-      <h5 id='nature-resource-name'>{mockMarker.natureResourceName} </h5>
-      <div className='popup-info-container'>
-        <p>Category: {mockResources[1].natureResourceCategory}</p>
-        <p>
-          Coordinates: [{mockMarker.location.lat},{mockMarker.location.long}]
+    <Popup className='popup-container'>
+      <div className='popup-header-container'>
+        <img className='resource-icon' src={fruitsAndVegetableIcon} alt='Icon for berries' />
+        <h5 id='nature-resource-name'>{mockMarker.natureResourceName} </h5>
+        <p id='date-user-info-text'>
+          Added by <b>Username</b> on <b>19/02/2020</b>
         </p>
+      </div>
+      <div className='popup-info-container'>
+        <p id='address-text'>
+          <b>Address</b> (lat: 60.192059, long:24.945831)
+        </p>
+        <p id='user-comment-text'>Comment from the user</p>
+        <a id='wikipedia-link' href='#'>
+          Link to Wikipedia
+        </a>
       </div>
     </Popup>
   );
